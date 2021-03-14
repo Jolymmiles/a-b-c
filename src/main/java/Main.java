4464f5dd490c66edeb2c216f7e94479d07016ec6
Boris Moiseev <bmoiseev@digital-spirit.ru>
2021-03-14 22:56:58 +0400

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        float c = sc.nextFloat();
-        float result = (a + b) * c;
-        System.out.println(result);
-        sc.close();
     }
 }
