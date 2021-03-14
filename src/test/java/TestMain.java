13a61a8cd65d378b617e46de958aa3f8094b8716
Boris Moiseev <bmoiseev@digital-spirit.ru>
2021-03-14 22:56:51 +0400

    init

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -28,7 +28,7 @@ public class TestMain {
     }
 
     @Test
-    public void testMainFirst() {
+    public void testMain1() {
         String data = "1\n2\n4.5";
         System.setIn(new ByteArrayInputStream(data.getBytes()));
         Main.main(null);
@@ -37,5 +37,50 @@ public class TestMain {
         Double actual = Double.parseDouble(result);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testMain2() {
+        String data = "82\n7162\n20.7836";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(null);
+        Double expected = 150556.39;
+        String result = outContent.toString().replaceAll("[\\r\\n]", "");
+        Double actual = Double.parseDouble(result);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMain3() {
+        String data = "240\n4260\n42.2751";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(null);
+        Double expected = 190237.95;
+        String result = outContent.toString().replaceAll("[\\r\\n]", "");
+        Double actual = Double.parseDouble(result);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMain4() {
+        String data = "955\n19202\n64.9732";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(null);
+        Double expected = 1309664.8;
+        String result = outContent.toString().replaceAll("[\\r\\n]", "");
+        Double actual = Double.parseDouble(result);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMain5() {
+        String data = "423\n-232312\n-3322.2421";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(null);
+        Double expected = 7.7039142E8;
+        String result = outContent.toString().replaceAll("[\\r\\n]", "");
+        Double actual = Double.parseDouble(result);
+        assertEquals(expected, actual);
+    }
+
 
 }
