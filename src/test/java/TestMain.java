52b623777e9ebeccc56bd73e9c932f4b14b63fad
Boris Moiseev <bmoiseev@digital-spirit.ru>
2021-03-14 22:51:30 +0400

    init

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    public TestMain() {
+        Locale.setDefault(new Locale("en", "US"));
+    }
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testMainFirst() {
+        String data = "1\n2\n4.5";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Main.main(null);
+        Double expected = 13.5;
+        String result = outContent.toString().replaceAll("[\\r\\n]", "");
+        Double actual = Double.parseDouble(result);
+        assertEquals(expected, actual);
+    }
+
+}
