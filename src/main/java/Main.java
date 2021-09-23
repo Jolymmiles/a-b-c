bdc405e42e9b371d43abef1dbb02884d2cd9b6b4
Валентин <valmak04@gmail.com>
2021-09-23 18:31:45 +0400

    Done 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt();
+        int A = sc.nextInt(), B = sc.nextInt();
+        float C = sc.nextFloat();
         System.out.print((A + B) * C);
     }
 }
